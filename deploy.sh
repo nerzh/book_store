bade7d0e129bd1a4ac2de5b323456d34f3cc825d
woodcrust <roboucrop@gmail.com>
2016-10-13 04:44:25 +0300

    clear

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git checkout .
-git pull origin master
-/etc/init.d/bookstore.inclouds.com.ua stop
-rm -Rf ./public/assets
-RAILS_ENV=production bin/rake assets:precompile
-RAILS_ENV=production bin/rake db:migrate
-/etc/init.d/bookstore.inclouds.com.ua start
